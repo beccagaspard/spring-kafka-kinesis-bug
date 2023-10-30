2607031c1950c6e4d04af095092fbf68d2bec7d6
beccagaspard <beccagaspard@carfax.com>
2023-10-30 15:34:35 -0400

    Initial commit - application fails to start with message "The bean 'kafkaExtendedPropertiesDefaultMappingsProvider', defined in class path resource [org/springframework/cloud/stream/binder/kinesis/config/ExtendedBindingHandlerMappingsProviderConfiguration.class], could not be registered. A bean with that name has already been defined in class path resource [org/springframework/cloud/stream/binder/kafka/config/ExtendedBindingHandlerMappingsProviderConfiguration.class] and overriding is disabled."

---
+++ src/main/java/com/example/springkafkakinesisbug/SpringKafkaKinesisBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkakinesisbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaKinesisBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaKinesisBugApplication.class, args);
+    }
+
+}
